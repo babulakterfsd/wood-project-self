bd4f2120dee66fdb68c030eaec694757922a6f3e
Babul Akter <babulakterfsd@gmail.com>
2023-07-13 12:59:18 +0600

    desktop done

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,15 +1,15 @@
-import { createEnv } from '@t3-oss/env-core';
-import { z } from 'zod';
+// import { createEnv } from '@t3-oss/env-core';
+// import { z } from 'zod';
 
-const ENV = createEnv({
-  clientPrefix: 'NEXT_PUBLIC_',
-  runtimeEnv: process.env,
-  client: {},
-  server: {
-    NODE_ENV: z
-      .enum(['development', 'production', 'testing'])
-      .default('production'),
-  },
-});
+// const ENV = createEnv({
+//   clientPrefix: 'NEXT_PUBLIC_',
+//   runtimeEnv: process.env,
+//   client: {},
+//   server: {
+//     NODE_ENV: z
+//       .enum(['development', 'production', 'testing'])
+//       .default('production'),
+//   },
+// });
 
-export default ENV;
+// export default ENV;
